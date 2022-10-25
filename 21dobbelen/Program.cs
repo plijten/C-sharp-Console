597a33e5e92dea77134eb51d03448f2f1deab80e
Peter Lijten <plijten@rocvantwente.nl>
2022-10-25 11:55:24 +0200

    21 dobbelstenen

---
+++ 21dobbelen/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace _21dobbelen
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int score = 0;
+            int worpen = 0;
+            Random random = new Random();
+            do
+            {
+                int dobbel = random.Next(1, 7);
+                if (score + dobbel <= 21)
+                {
+                    score += dobbel;
+                }
+
+                worpen++;
+
+            } while (score < 21);
+
+            Console.WriteLine($"Aantal worpen {worpen}");
+        }
+    }
+}
