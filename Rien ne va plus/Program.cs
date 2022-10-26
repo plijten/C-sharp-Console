372d7ce644a93f530e190130a91f9de1b1dbe517
Peter Lijten <plijten@rocvantwente.nl>
2022-10-26 11:39:12 +0200

    Rien ne va plus, 1 getal.

---
+++ Rien ne va plus/Program.cs
@@ -0,0 +1,82 @@
+﻿int chips = 10;
+string input = "";
+int number = 0;
+int chipsOnNumber = 0;
+bool stop = false;
+
+Random random = new Random();
+do
+{
+    // Inzet door de gebruiker laten invoeren.
+    do
+    {
+        Console.WriteLine("Op welk getal wil je chips inzetten?");
+        input = Console.ReadLine();
+
+        if (!int.TryParse(input, out number))
+        {
+            if (input.ToLower() == "stop")
+                stop = true;
+            else
+                Console.WriteLine("Ongeldige invoer.");
+        }
+        else
+        {
+            Console.WriteLine("Hoeveel chips wil je inzetten?");
+            input = Console.ReadLine();
+
+            if (!int.TryParse(input, out chipsOnNumber))
+            {
+                if (input.ToLower() == "stop")
+                    stop = true;
+                else
+                    Console.WriteLine("Ongeldige invoer.");
+            }
+        }
+
+        if (chipsOnNumber <= 0 && stop == false)
+        {
+            Console.WriteLine("Aantal chips moet groter zijn zijn nul");
+        }
+
+        if (chipsOnNumber > chips && stop == false)
+        {
+            Console.WriteLine($"Je hebt maar {chips} beschikbaar.");
+        }
+
+        // Kan niet meer chips inzetten.
+        if (chipsOnNumber == chips && stop == false)
+        {
+            Console.WriteLine("Rien ne va plus");
+            stop = true;
+        }
+
+    } while (!stop);
+
+    // Trekking
+    int score = random.Next(1, 37);
+    
+    Console.Clear();
+    Console.WriteLine($"het balletje is op {score} gevallen.");
+
+    if (score == number)
+    {
+        Console.WriteLine("Je hebt dit getal gekozen en 35 extra punten verdient.");
+        chips += 35;
+    }
+    else
+    {
+        Console.WriteLine("Helaas je hebt je inzet verloren.");
+        chips -= chipsOnNumber;
+    }
+
+    
+    Console.WriteLine("Druk op een toets om verder te spelen.");
+    Console.ReadKey();
+
+    stop = false;
+    Console.Clear();
+
+} while (chips > 0);
+
+Console.WriteLine("Al je chips zijn op, het spel is afgelopen.");
---
+++ Yatzee/Program.cs
@@ -0,0 +1,121 @@
+﻿int worpen = 0;
+int score = 0;
+int[] dobbelstenen = new int[5];
+string[] steenstatus = new string[5] { "l", "l", "l", "l", "l" };
+
+do
+{
+    ToonMenu();
+    string actie = Console.ReadLine();
+
+    switch (actie.ToUpper())
+    {
+        case "W":
+            WerpStenen();
+            worpen++;
+            break;
+        case "U":
+            BepaalUitslag();
+            break;
+        default:
+            Console.WriteLine("Geen geldige keuze");
+            break;
+    }
+
+} while (worpen <= 3);
+
+void WerpStenen()
+{
+    Random r = new Random();
+    for (int i = 0; i < 5; i++)
+    {
+        if (steenstatus[i] == "l")
+        {
+            dobbelstenen[i] = r.Next(1, 7);
+        }        
+    }
+
+    Console.WriteLine(String.Join(",", dobbelstenen));
+    Console.WriteLine("Stenen vastzetten of losmaken.");
+    steenstatus = Console.ReadLine().Split(",");
+}
+
+void BepaalUitslag()
+{
+    // Sorteer de dobbelstenen van laag naar hoog.
+    Array.Sort(dobbelstenen);
+
+    //Bepaal Yatzee
+    score = BepaalYatzee(dobbelstenen);
+
+    //Bepaald 4 of a kind
+    if (score == 0)
+    {
+        score = BepaalFourOfAKind(dobbelstenen);
+    }
+    //Bepaal 3 of a kind
+    //if (score == 0)
+        //score = BepaalThreeOfAKind();
+
+    //Bepaal grote straat
+
+    //Bepaal kleine straat
+}
+
+
+
+static void ToonMenu()
+{
+    Console.Clear();
+
+    Console.WriteLine("----------------------------------------");
+    Console.WriteLine("Werpen => W");
+    Console.WriteLine("Uitslag => U");
+    Console.WriteLine("Vastzetten => H");
+    Console.WriteLine("Losmaken => L");
+    Console.WriteLine("");
+    Console.WriteLine("Voorbeeld Vastzetten (h,h,l,l,l) om de eerste 2 stenen vast te zetten");
+    Console.WriteLine("----------------------------------------");
+
+}
+
+
+static int BepaalYatzee(int[] dobbelstenen)
+{
+    if (dobbelstenen[0] == dobbelstenen[1] && dobbelstenen[1] == dobbelstenen[2] &&
+        dobbelstenen[2] == dobbelstenen[3] && dobbelstenen[3] == dobbelstenen[4])
+    {
+        return 50;
+    }
+
+    return 0;
+}
+
+static int BepaalFourOfAKind(int[] dobbelstenen)
+{
+    if (dobbelstenen[0] == dobbelstenen[1] && dobbelstenen[1] == dobbelstenen[2] &&
+        dobbelstenen[2] == dobbelstenen[3])
+    {
+        return 40;
+    }
+
+    if (dobbelstenen[1] == dobbelstenen[2] && dobbelstenen[2] == dobbelstenen[3] &&
+        dobbelstenen[3] == dobbelstenen[4])
+    {
+        return 40;
+    }
+
+    return 0;
+}
+
+int BepaalThreeOfAKind(int[] dobbelstenen)
+{
+    if ((dobbelstenen[0] == dobbelstenen[1] && dobbelstenen[1] == dobbelstenen[2]) ||
+        (dobbelstenen[1] == dobbelstenen[2] && dobbelstenen[2] == dobbelstenen[3]) ||
+        (dobbelstenen[2] == dobbelstenen[3] && dobbelstenen[3] == dobbelstenen[4]))
+    {
+        return 30;
+    }
+
+    return 0;
+}
